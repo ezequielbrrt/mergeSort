60f3407ce48ded596ea28a655d5c46742f5d4403
ezequiel <ezequielbrrt@gmailcom>
2016-02-21 21:50:26 -0600

    Main terminado

--- mergeSort.py
+++ mergeSort.py
@@ -1,9 +1,22 @@
 #! /bin/python
+# -*- coding: utf-8 -*-
 
-def mergeSort():
-	pass
+
+def mergeSort(arreglo):
+	return arreglo
 
 def main():
+	salir = True
+	valores = []
 	print "Merge Sort"
+	while salir:
+		try: 
+			numeros = int(raw_input("Escribe los números a ordenar: "))
+			valores.append(numeros)
+		except:
+			print "Iniciando el Algoritmo"
+			salir = False
+	print mergeSort(valores)
 
 main()
+
