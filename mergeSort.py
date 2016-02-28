6f6aa6d48ea77329a1ca9cad7bddee6ecfd9d707
ezequiel <ezequielbrrt@gmailcom>
2016-02-27 20:55:21 -0600

    Algoritmo de merge sort

--- mergeSort.py
+++ mergeSort.py
@@ -3,7 +3,32 @@
 
 
 def mergeSort(arreglo):
-	return arreglo
+    if len(arreglo) > 1:
+        mitad = len(arreglo)
+        parte_izquierda = arreglo[:mitad]
+        parte_derecha = arreglo[mitad:]
+        mergeSort(parte_izquierda)
+        mergeSort(parte_derecha)
+        i=0
+        j=0
+        k=0
+        while i < len(parte_izquierda) and j < len(parte_derecha):
+            if parte_izquierda[i] < parte_derecha[j]:
+                arreglo[k]=parte_izquierda[i]
+                i=i+1
+            else:
+                arreglo[k]=parte_derecha[j]
+                j=j+1
+            k=k+1
+        while i < len(parte_izquierda):
+            arreglo[k]=parte_izquierda[i]
+            i=i+1
+            k=k+1
+        while j < len(parte_derecha):
+            arreglo[k]=parte_derecha[j]
+            j=j+1
+            k=k+1
+    return arreglo
 
 def main():
 	salir = True
